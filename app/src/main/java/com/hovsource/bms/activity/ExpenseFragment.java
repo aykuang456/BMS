1cb3de882d76bca606a7045d2e205e4cd01a5057
Administrator <405576846@qq.com>
2016-08-01 17:44:57 +0800

    添加事件信息

--- app/src/main/java/com/hovsource/bms/activity/ExpenseFragment.java
+++ app/src/main/java/com/hovsource/bms/activity/ExpenseFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.hovsource.bms.activity.bms.R;
 
@@ -31,4 +34,58 @@ public class ExpenseFragment extends BaseFragment {
     private void initView(View view){
 
     }
+
+    public class DataAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder holder;
+
+            return null;
+        }
+    }
+
+    /**存放控件*/
+    public final class ViewHolder{
+        public TextView name;
+        public TextView flow;
+        public TextView schedule;
+        public TextView amount;
+        public TextView form;
+    }
+
+    /**
+     *  条目单击事件
+     */
+    AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
+    };
+
+    /**
+     *  条目长按事件
+     */
+    AdapterView.OnItemLongClickListener onItemLongClickListener = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+            return false;
+        }
+    };
 }
