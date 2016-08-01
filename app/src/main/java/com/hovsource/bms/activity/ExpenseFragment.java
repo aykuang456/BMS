ad66aa300720b62a886e73233cc68b6e5ae01889
Administrator <405576846@qq.com>
2016-08-01 17:02:43 +0800

    删除错误信息

---
+++ app/src/main/java/com/hovsource/bms/activity/ExpenseFragment.java
@@ -0,0 +1,34 @@
+package com.hovsource.bms.activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hovsource.bms.activity.bms.R;
+
+
+public class ExpenseFragment extends BaseFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_expense, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view){
+
+    }
+}
